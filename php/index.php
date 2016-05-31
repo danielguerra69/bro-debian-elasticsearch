b5abc5244fda9a3e884dea4a7837e8e84a403523
danielguerra69 <daniel.guerra69@gmail.com>
2016-05-31 20:04:48 +0200

    final content fix
    index php fixed
    xinetd forecsic fixed forgotten output file
    bro-forensic changed from tee to bro -w PCAP

--- php/index.php
+++ php/index.php
@@ -1,3 +1,9 @@
 <?php
-echo exec('tcpdump -r ' . $_GET["pcap_file"] . ' -w - ' . $_GET["proto"] . ' and host '.$_GET["orig_h"] . ' and port ' . $_GET["orig_p"] . ' and host ' . $_GET["resp_h"] . ' and port ' . $_GET["resp_p"]);
+$command="/usr/sbin/tcpdump -r ". $_GET["pcap_file"] . ' -w - ' . $_GET["proto"] . ' and host '.$_GET["orig_h"] . ' and port ' . $_GET["orig_p"] . ' and host ' . $_GET["resp_h"] . ' and port ' . $_GET["resp_p"];
+$filename = $_GET["proto"]."-".$_GET["orig_h"].":".$_GET["orig_p"]."x".$_GET["resp_h"].":".$_GET["resp_p"];
+header("Content-Disposition: attachment; filename=\"$filename.pcap\"");
+header("Content-Type:application/octet-stream");
+$contents = shell_exec($command);
+header('Content-Length: '.strlen($contents));
+print $contents;
 ?>
