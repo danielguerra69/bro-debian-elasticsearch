35a744e67aed483b5413cc446865d73269d10b88
danielguerra69 <daniel.guerra69@gmail.com>
2016-09-29 08:38:56 +0200

    sed fix for bro forensic
    now you can use export DOCKERHOST=“127.0.0.1:8080”

--- scripts/bro-forensic.sh
+++ scripts/bro-forensic.sh
@@ -4,13 +4,13 @@
 #file extraction
 export DOCKERHOST=$1
 cp /usr/local/bro/share/bro/bro-extra/extract_files_template.bro /usr/local/bro/share/bro/base/files/extract/main.bro
-sed -i "s:DOCKERHOST:${DOCKERHOST}:" /usr/local/bro/share/bro/base/files/extract/main.bro
+sed -i "s/DOCKERHOST/${DOCKERHOST}/g" /usr/local/bro/share/bro/base/files/extract/main.bro
 
 #pcap file and settings
 export PCAPFILE=`tempfile -d /bro/pcap -p bro- -s .pcap`
 cp /usr/local/bro/share/bro/bro-extra/conn_pcap_template.bro /usr/local/bro/share/bro/bro-extra/conn_pcap.bro
 sed -i "s:PCAPFILE:${PCAPFILE}:" /usr/local/bro/share/bro/bro-extra/conn_pcap.bro
-sed -i "s:DOCKERHOST:${DOCKERHOST}:" /usr/local/bro/share/bro/bro-extra/conn_pcap.bro
+sed -i "s/DOCKERHOST/${DOCKERHOST}/g" /usr/local/bro/share/bro/bro-extra/conn_pcap.bro
 # set file permissions for apache
 chown www-data:www-data ${PCAPFILE}
 #cmd
